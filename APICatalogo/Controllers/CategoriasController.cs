bd41b16fc933ce55793ea3729b0b6086cee08754
Arthur Bomfim <arthurbao10@gmail.com>
2024-12-05 14:02:43 -0300

    get catgorias falta terminar get com o id

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,46 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //Retornando Todas as categorias
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.ToList();
+            if (categorias is null)
+            {
+                return NotFound(" >> Categorias Não Encontradas <<");
+            }
+
+            return Ok(categorias);
+        }
+
+        // Retornanndo com Id
+        [HttpGet("{id:int}")]
+        public ActionResult Categoria Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound(" >> Categoria Não Encontrada <<");
+            }
+            Ok(categoria);
+        }
+    }
+}
