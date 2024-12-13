fec8283a81a8d57607848cc9082e8406ca590d72
Arthur Bomfim <arthurbao10@gmail.com>
2024-12-13 11:14:21 -0300

    a

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c1701065127cd396911dadc9e8b1105c7cfb50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f7368cc11f3b264d022381dbaf8366b0a81705")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
